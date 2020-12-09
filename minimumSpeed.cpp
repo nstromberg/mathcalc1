b25ce959ac54b3d08c3baeb19e5f0f40fe8d28ac
TabithaHolloway <37598390+TabithaHolloway@users.noreply.github.com>
2020-12-08 22:01:01 -0500

    commented code opens a new window if min speed is selected

--- minimumSpeed.cpp
+++ minimumSpeed.cpp
@@ -12,3 +12,48 @@ MinimumSpeed::~MinimumSpeed()
 {
     delete ui;
 }
+
+
+/** commented code allows user to open new window when min speed is selected
+//NOTE: CODE DOES NOT WORK PROPERLY, BUT THIS WILL HELP YOU OPEN A NEW WINDOW WHEN A FORMULA IS SELECTED
+
+#include "minimumSpeed.h"
+#include "ui_minimumSpeed.h"
+#include <QDebug>
+MinimumSpeed::MinimumSpeed(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::minimumSpeed_UI)
+{
+    ui->setupUi(this);
+    this->hide();
+}
+
+MinimumSpeed::~MinimumSpeed()
+{
+    delete ui;
+}
+
+
+void MinimumSpeed::ShowMinSpeed()
+{
+    this->show();
+}
+
+void MinimumSpeed::HideMinSpeed() {
+    this->hide();
+}
+//captures values typed for the acceleration drag coefficient
+void MinimumSpeed::captureAccelVal() {
+    QLineEdit* ran =  ui->AccelDragFLineEdit;
+    qDebug() << ran->text();
+}
+
+void MinimumSpeed::on_AccelDragFLineEdit_textChanged()
+{
+    captureAccelVal();
+}
+
+
+
+
+*/
