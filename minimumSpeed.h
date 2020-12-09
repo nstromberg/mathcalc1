410bf99d44fa2d9652c7afcdfe57308e6dd69b11
TabithaHolloway <37598390+TabithaHolloway@users.noreply.github.com>
2020-12-08 21:53:01 -0500

    Commented code allow new tab to open for min speed

--- minimumSpeed.h
+++ minimumSpeed.h
@@ -19,3 +19,41 @@ private:
     Ui::minimumSpeed_UI *ui;
 };
 #endif // MINIMUMSPEED_H
+
+
+/** this code will allow you to open a new tab for the min speed when the user selects the formula
+
+#ifndef MINIMUMSPEED_H
+#define MINIMUMSPEED_H
+
+#include <QWidget>
+#include <ui_minimumSpeed.h>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MinimumSpeed; }
+QT_END_NAMESPACE
+
+class MinimumSpeed : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MinimumSpeed(QWidget *parent = nullptr);
+    ~MinimumSpeed();
+    void ShowMinSpeed();
+    void HideMinSpeed();
+    void captureAccelVal();
+
+private:
+    Ui::minimumSpeed_UI *ui;
+private slots:
+    void on_AccelDragFLineEdit_textChanged();
+
+
+
+friend class FormulaLayout;
+};
+#endif // MINIMUMSPEED_H
+
+
+
+*/
